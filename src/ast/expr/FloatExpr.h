3c0c223b6ed5b01740edc16f8dc1ce03a618b816
Rafael Campos Nunes <rafaelnunes@engineer.com>
2016-08-07 19:40:48 -0300

    Implementation of the float operator completed
        The implementation of the $ operator, known in Brain as the float operator is now complete. Thus we can express float numbers in Brain with ease. ++++$+++++++.+++.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/ast/expr/FloatExpr.h
@@ -0,0 +1,46 @@
+/* This is the source code of Brain Programming Language.
+ * It is licensed under GNU GPL v. 3 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Brain, 2016.
+ */
+
+#ifndef FLOAT_EXPR_H
+#define FLOAT_EXPR_H
+
+#include <llvm/Transforms/Utils/BuildLibCalls.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Module.h>
+
+#include <iostream>
+
+#include "Expr.h"
+
+/**
+ * @brief The FloatExpr class behaves just like the output expression, but it
+ * instead of print an integer number it will use io.c to divide the number by
+ * 100 and print it.
+ */
+class FloatExpr : public Expr
+{
+public:
+    FloatExpr() {}
+    ~FloatExpr() {}
+    /**
+     * @brief code_gen
+     * @param M
+     * @param B
+     */
+    void code_gen(llvm::Module *M, llvm::IRBuilder<> &B);
+    /**
+     * @brief debug_description
+     * @param level
+     */
+    void debug_description(int level);
+    /**
+     * @brief ast_code_gen
+     */
+    void ast_code_gen();
+};
+
+#endif // FLOAT_EXPR_H
--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -15,8 +15,9 @@
 #include "../ast/expr/DebugExpr.h"
 #include "../ast/expr/BreakExpr.h"
 #include "../ast/expr/IfExpr.h"
+#include "../ast/expr/FloatExpr.h"
 
-// Forward declaration of static members
+// Forward declaration of a static member
 static bool has_done_then = false;
 
 void Parser::parse(std::vector<Expr *> &exprs, int level)
@@ -24,128 +25,134 @@ void Parser::parse(std::vector<Expr *> &exprs, int level)
     char c = 0;
     while ((c = _data[_index++]))  {
         Expr *expr = nullptr;
-	if (ArgsOptions::instance()->get_optimization() == BO_IS_OPTIMIZING_O1 && !exprs.empty()) {
+        if (ArgsOptions::instance()->get_optimization() ==
+                BO_IS_OPTIMIZING_O1 && !exprs.empty()) {
+
             Expr *last_expression = exprs.back();
-            	if (last_expression->update_expression(c)) {
-  		    continue;
-		}
-	}
+            if (last_expression->update_expression(c)) {
+                continue;
+            }
+        }
 
-	switch (c)
-	{
-	case TT_SHIFT_LEFT:
+        switch (c)
+        {
+        case TT_SHIFT_LEFT:
             expr = new ShiftExpr(-1);
-	    break;
-	case TT_SHIFT_RIGHT:
-            expr = new ShiftExpr(1);
-	    break;
-	case TT_INCREMENT:
+            break;
+        case TT_SHIFT_RIGHT:
+             expr = new ShiftExpr(1);
+            break;
+        case TT_INCREMENT:
             expr = new IncrementExpr(1);
-	    break;
-	case TT_DECREMENT:
+            break;
+        case TT_DECREMENT:
             expr = new IncrementExpr(-1);
-	    break;
-	case TT_OUTPUT:
+            break;
+        case TT_OUTPUT:
             expr = new OutputExpr();
-	    break;
-	case TT_INPUT:
+            break;
+        case TT_INPUT:
             expr = new InputExpr();
-	    break;
-	case TT_BEGIN_WHILE:
-	{
+            break;
+        case TT_BEGIN_WHILE:
+        {
             std::vector<Expr *> loop_expression;
-	    parse(loop_expression, level + 1);
+            parse(loop_expression, level + 1);
             expr = new LoopExpr(loop_expression, LT_WHILE);
-	    break;
-	}
-	case TT_END_WHILE:
+            break;
+        }
+        case TT_END_WHILE:
         {
-	    if (level > 0) {
-	        // exit the recursivity.
-		return;
-	    }
+            if (level > 0) {
+                // exit the recursivity.
+                return;
+            }
 
-	    break;
-        } 
-	case TT_BEGIN_FOR:
-	{
+            break;
+        }
+        case TT_BEGIN_FOR:
+        {
             std::vector<Expr *> loop_expression;
-	    parse(loop_expression, level + 1);
+            parse(loop_expression, level + 1);
             expr = new LoopExpr(loop_expression, LT_FOR);
-	    break;
-	}
-	case TT_END_FOR:
-        {
-	    if (level > 0) {
-	        // exit the recursivity.
-		return;
-	    }
-
-	    break;
+            break;
         }
-	case TT_IF_THEN:
-	{
+        case TT_END_FOR:
+        {
+            if (level > 0) {
+                // exit the recursivity.
+                return;
+            }
+
+            break;
+        }
+        case TT_IF_THEN:
+        {
             std::vector<Expr *> if_expression;
-	    parse(if_expression, level + 1);
+            parse(if_expression, level + 1);
             expr = new IfExpr(if_expression);
-	    break;
-	}
-	case TT_IF_ELSE:
+            break;
+        }
+        case TT_IF_ELSE:
         {
-	    if (!has_done_then) {
-	        if (level == 0) {
-		    break;
-		}
+            if (!has_done_then) {
+                if (level == 0) {
+                    break;
+                }
 
-		_index--; // move one step back to read the ':' again
-		has_done_then = true;
-		return; // return to exit the 'then' recursivity
-	    }
+                _index--; // move one step back to read the ':' again
+                has_done_then = true;
+                return; // return to exit the 'then' recursivity
+            }
 
-	    // do the else
-	    if (!exprs.empty()) {
+            // do the else
+            if (!exprs.empty()) {
                 Expr *expr = exprs.back();
-		if (expr->expression_category() == ET_BRANCH) {
+
+                if (expr->expression_category() == ET_BRANCH) {
                     std::vector<Expr *> else_expression;
-		    parse(else_expression, level + 1);
+                    parse(else_expression, level + 1);
                     ((IfExpr *)expr)->set_else(else_expression);
-		}
-	    }
+                }
+            }
 
-	    has_done_then = false; // reset the flag
-	    break;
+            has_done_then = false; // reset the flag
+            break;
         }
-	case TT_IF_END:
+        case TT_IF_END:
         {
-	    if (level > 0) {
-		return;
-	    }
+            if (level > 0) {
+                return;
+            }
 
-	    break;
+            break;
         }
-	case TT_MUL:
+        case TT_MUL:
             expr = new ArithmeticExpr(AT_MUL);
-	    break;
-	case TT_DIV:
+            break;
+        case TT_DIV:
             expr = new ArithmeticExpr(AT_DIV);
-	    break;
-	case TT_REM:
+            break;
+        case TT_REM:
             expr = new ArithmeticExpr(AT_REM);
-	    break;
-	case TT_DEBUG:
+            break;
+        case TT_DEBUG:
             expr = new DebugExpr();
-	    break;
-	case TT_BREAK:
+            break;
+        case TT_BREAK:
             expr = new BreakExpr();
-	    break;
-	default:
-	    // Ignored character
-	    continue;
-	}
+            break;
+        case TT_FLOAT:
+            expr = new FloatExpr();
+            break;
+        default:
+            // Ignored character
+            continue;
+        }
 
-	if (expr) {
-	    exprs.push_back(expr);
-	}
+        if (expr) {
+            exprs.push_back(expr);
+        }
     }
 }
 
@@ -154,7 +161,7 @@ void Parser::code_gen(llvm::Module *M, llvm::IRBuilder<> &B)
     ASTInfo::instance()->code_gen(M, B);   
 
     for (auto& expr : _exprs) {
-	expr->code_gen(M, B);
+        expr->code_gen(M, B);
     }
 }
 
